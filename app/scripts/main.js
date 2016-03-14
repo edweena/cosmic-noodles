d13a724c7914adf3c96dfea2b1ebae235d299b43
James Pants <james@rds.zone>
2016-03-14 11:12:34 +0100

    test

---
+++ app/scripts/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+class Piece {
+	constructor(){
+		this.init();
+	}
+
+	// ------------------------------------------------
+	// Kick off
+	//
+	
+	init(){
+		console.log('What up son?');
+	}
+	
+};
+
+
+
+
+
+let piece = new Piece();
